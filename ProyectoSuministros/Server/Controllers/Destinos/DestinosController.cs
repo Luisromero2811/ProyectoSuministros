6bea92a547fea7589ce283df09b275c0d3a1a49c
Luisromero2811 <19170067@uttcampus.edu.mx>
2024-08-05 12:42:12 -0600

    "Subí toda la estructura del front end del proyecto, ajustes de menú"

--- ProyectoSuministros/Server/Controllers/Destinos/DestinosController.cs
+++ ProyectoSuministros/Server/Controllers/Destinos/DestinosController.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProyectoSuministros.Shared.Modelos;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ProyectoSuministros.Server.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DestinosController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext context;
+
+        public DestinosController(ApplicationDbContext context)
         {
-            return View();
+            this.context = context;
         }
+
+        //Creación de destinos
+        [HttpPost("crear")]
+        public async Task<ActionResult> PostDestino([FromBody] Destinos destinos)
+        {
+            if (destinos is null)
+            {
+                return BadRequest();
+            }
+
+            //Si el destino viene en ceros del front lo agregamos como nuevo sino lo actualizamos
+            if (destinos.ID == 0)
+            {
+                context.Add(destinos);
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                context.Update(destinos);
+                await context.SaveChangesAsync();
+            }
+
+            return Ok();
+
+        }
+
     }
 }
 
